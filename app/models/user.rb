66b809bd95dea251d97ded3393d51d2695eefa4d
florian bernier <florianbernier.soa@gmail.com>
2024-02-27 11:57:00 +0100

    add validates to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
 end
