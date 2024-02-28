c78f0e0771d879d75e4e54d33f0caf49a7dd70fd
Romain Bernier <romain.bernier@live.fr>
2024-02-28 21:23:13 +0100

    bookings seed, booking model change, show rework and maps into show

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,29 @@ mathieu = User.create!(username: "mathieu", email: "mathieu@gmail.com", password
 
 puts "seeded #{User.count} users"
 
-Game.create!(name: "Poker", game_type: "Card Game", content: "Texas Hold'em!! Yeehaw!!", price: 5, user: florian)
-Game.create!(name: "Poker", game_type: "Card Game", content: "Ma malette de poker est meilleure que Flo", price: 7, user: romain)
-Game.create!(name: "Monopoly", game_type: "Board Game", content: "Destructeur de relations", price: 5, user: marine)
-Game.create!(name: "Animal Crossing account", game_type: "Digital Game", content: "my world", price: 15, user: knnll)
-Game.create!(name: "Un marteau", game_type: "Sport Game", content: "Dans ta tronche", price: 0, user: cedric)
-Game.create!(name: "Basketball", game_type: "Sport Game", content: "C'est un beau ballon", price: 2, user: perrine)
-Game.create!(name: "Pokemon GBA", game_type: "Digital Game", content: "Gotta catch them all", price: 40, user: mathieu)
+florianGame = Game.create!(name: "Poker", game_type: "Card Game", content: "Texas Hold'em!! Yeehaw!!", price: 5, user: florian)
+romainGame = Game.create!(name: "Poker", game_type: "Card Game", content: "Ma malette de poker est meilleure que Flo", price: 7, user: romain)
+knnllGame = Game.create!(name: "Animal Crossing account", game_type: "Digital Game", content: "my world", price: 15, user: knnll)
+cedricGame = Game.create!(name: "Un marteau", game_type: "Sport Game", content: "Dans ta tronche", price: 0, user: cedric)
+marineGame = Game.create!(name: "Monopoly", game_type: "Board Game", content: "Destructeur de relations", price: 5, user: marine)
+perrineGame = Game.create!(name: "Basketball", game_type: "Sport Game", content: "C'est un beau ballon", price: 2, user: perrine)
+mathieuGame = Game.create!(name: "Pokemon GBA", game_type: "Digital Game", content: "Gotta catch them all", price: 40, user: mathieu)
 
 puts "seeded #{Game.count} games"
+
+Booking.create!(game: florianGame, user: romain, message: "kikoo", status: "pending")
+Booking.create!(game: florianGame, user: knnll, message: "kikoo", status: "pending")
+Booking.create!(game: romainGame, user: knnll, message: "kikoo", status: "pending")
+Booking.create!(game: romainGame, user: cedric, message: "kikoo", status: "pending")
+Booking.create!(game: knnllGame, user: cedric, message: "kikoo", status: "pending")
+Booking.create!(game: knnllGame, user: marine, message: "kikoo", status: "pending")
+Booking.create!(game: cedricGame, user: marine, message: "kikoo", status: "pending")
+Booking.create!(game: cedricGame, user: perrine, message: "kikoo", status: "pending")
+Booking.create!(game: marineGame, user: perrine, message: "kikoo", status: "pending")
+Booking.create!(game: marineGame, user: mathieu, message: "kikoo", status: "pending")
+Booking.create!(game: perrineGame, user: mathieu, message: "kikoo", status: "pending")
+Booking.create!(game: perrineGame, user: florian, message: "kikoo", status: "pending")
+Booking.create!(game: mathieuGame, user: florian, message: "kikoo", status: "pending")
+Booking.create!(game: mathieuGame, user: romain, message: "kikoo", status: "pending")
+
+puts "seeded #{Booking.count} bookings"
